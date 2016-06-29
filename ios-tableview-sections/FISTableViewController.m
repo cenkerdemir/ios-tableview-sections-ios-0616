81138fadd9f58e0d9e22feab0b59b19bddabbb07
github username <github email address>
2016-06-29 12:11:21 -0400

    complete except for the advanced part

--- ios-tableview-sections/FISTableViewController.m
+++ ios-tableview-sections/FISTableViewController.m
@@ -16,32 +16,69 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    FISStudent *student1 = [[FISStudent alloc] init];
+    student1.name = @"JOKER";
+    student1.favoriteThings = @[@"Sleep",@"Jokes",@"Fake shoes"];
+    student1.favoriteFood = @"joker food";
+    student1.favoriteColor = @"joker color";
+    student1.favoriteMusicalArtist = @"joker's band";
+    student1.favoriteGame = @"joker's game";
+    
+    
+    FISStudent *student2 = [[FISStudent alloc] init];
+    student2.name = @"BATMAN";
+    student2.favoriteThings = @[@"Batmobile",@"Catwoman",@"Farts"];
+    
+    FISStudent *student3 = [[FISStudent alloc] init];
+    student3.name = @"ROBIN";
+    student3.favoriteThings = @[@"Batman",@"Silicon Valley Show",@"Objective-c"];
+    
+    FISStudent *student4 = [[FISStudent alloc] init];
+    student4.name = @"HELLBOY";
+    student4.favoriteThings = @[@"Red Color",@"Cigar",@"Mr.robot"];
+    
+    self.students = @[student1, student2, student3, student4];
     
 }
 
 #pragma mark - Table view data source
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-#warning Potentially incomplete method implementation.
+//#warning Potentially incomplete method implementation.
     // Return the number of sections.
-    return 0;
+    return (NSInteger)[self.students count];
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-#warning Incomplete method implementation.
+//#warning Incomplete method implementation.
     // Return the number of rows in the section.
-    return 0;
+    return (NSInteger)[((FISStudent *)(self.students[(NSUInteger)section])).favoriteThings count];
 }
 
-/*
+
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
     
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"expandingCell" forIndexPath:indexPath];
     // Configure the cell...
+    
+    NSArray *favoriteThings = [[self.students objectAtIndex:(NSUInteger)indexPath.section] favoriteThings];
+    
+    cell.textLabel.text = [favoriteThings objectAtIndex:(NSUInteger)indexPath.row];
+    
+    cell.detailTextLabel.text = [NSString stringWithFormat:@"%li", indexPath.row];
     
     return cell;
 }
-*/
+
+-(NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
+
+    return [self.students[(NSUInteger)section] name];
+
+}
+
+-(NSInteger)tableView:(UITableView *)tableView indentationLevelForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return 2;
+}
 
 /*
 // Override to support conditional editing of the table view.
